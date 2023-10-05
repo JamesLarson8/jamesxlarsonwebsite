a8cc55468db1e7002fb23d1032b21989b38d2331
JamesLarson8 <jmlarson8@gmail.com>
2023-10-05 16:41:53 -0400

    color scheme

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@
 window.onload = () => {
     console.log("Window loaded");
     const greeting = 'James Larson';
-    const test = '';
+    const test = 'This site is currently under development';
     console.log("Greeting set");
     document.getElementById('greeting').textContent = greeting;
     document.getElementById('test').textContent = test;
--- docs/main.ts
+++ docs/main.ts
@@ -1,7 +1,7 @@
 window.onload = (): void => {
     console.log("Window loaded");
     const greeting: string = 'James Larson';
-    const test: string = '';
+    const test: string = 'This site is currently under development';
     console.log("Greeting set");
     document.getElementById('greeting')!.textContent = greeting;
     document.getElementById('test')!.textContent = test;
