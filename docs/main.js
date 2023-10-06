df86313806faf0ee8052a4f0284e2ca3de1a1425
JamesLarson8 <jmlarson8@gmail.com>
2023-10-06 14:07:42 -0400

    under development message

--- docs/main.js
+++ docs/main.js
@@ -2,8 +2,8 @@
 window.onload = () => {
     console.log("Window loaded");
     const greeting = 'James Larson';
-    const test = 'This site is currently under development';
+    const devmessage = 'This site is currently under development';
     console.log("Greeting set");
     document.getElementById('greeting').textContent = greeting;
-    document.getElementById('test').textContent = test;
+    document.getElementById('test').textContent = devmessage;
 };
--- docs/main.ts
+++ docs/main.ts
@@ -1,8 +1,8 @@
 window.onload = (): void => {
     console.log("Window loaded");
     const greeting: string = 'James Larson';
-    const test: string = 'This site is currently under development';
+    const devmessage: string = 'This site is currently under development';
     console.log("Greeting set");
     document.getElementById('greeting')!.textContent = greeting;
-    document.getElementById('test')!.textContent = test;
+    document.getElementById('test')!.textContent = devmessage;
 };
