82e802e0bd71d0c8b2a7b07eb2e8baf8dbbc8424
JamesLarson8 <jmlarson8@gmail.com>
2023-10-12 21:50:38 -0400

    updated website

--- docs/main.ts
+++
@@ -1,8 +0,0 @@
-window.onload = (): void => {
-    console.log("Window loaded");
-    const greeting: string = 'James Larson';
-    const devmessage: string = 'This site is currently under development';
-    console.log("Greeting set");
-    document.getElementById('greeting')!.textContent = greeting;
-    document.getElementById('test')!.textContent = devmessage;
-};
